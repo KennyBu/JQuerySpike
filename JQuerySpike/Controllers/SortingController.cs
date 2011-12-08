b13f29bc5df1ad58f4e0a69e1e0370960ee2f50a
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-12-08 17:22:34 -0500

    initial checkin

---
+++ JQuerySpike/Controllers/SortingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQuerySpike.Controllers
+{
+    public class SortingController : Controller
+    {
+        //
+        // GET: /Sorting/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public EmptyResult Index(List<int> listIds)
+        {
+            var firstId = listIds.FirstOrDefault();
+
+            return new EmptyResult();
+        }
+
+    }
+}
